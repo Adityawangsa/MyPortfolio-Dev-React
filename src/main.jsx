f519ada0f040aa7b920659e6dcb13e10086b61c4
Adityawangsa <adityahawari63@gmail.com>
2026-05-14 02:48:02 +0700

    style: Mengatur lebar layar untuk 4 ukuran (mobile, table, laptop, dan desktop)

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './style.css'
-import App from './App.jsx'
-import Navbar from './components/Navbar.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import App from "./App.jsx";
+import Navbar from "./components/Navbar.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="container mx-auto px-4">
+    <div className="container max-w-full md:max-w-3xl lg:max-w-5xl 2xl:max-w-7xl mx-auto px-4">
       <Navbar></Navbar>
       <App />
     </div>
   </StrictMode>,
-)
+);
