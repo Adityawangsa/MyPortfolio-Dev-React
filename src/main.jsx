9c4106c5f891e7420b1d76c2b5e675ea43c0b06a
Adityawangsa <adityahawari63@gmail.com>
2026-05-14 13:03:25 +0700

    Menambahkan komentar terdahap beberapa layout, komponen, libary yang digunakan

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
+// Bawaan react
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
+
+// Memasukkan layout dan komponen
 import App from "./App.jsx";
 import Navbar from "./components/Navbar.jsx";
 
+// Penggunaan remixicon
+import "remixicon/fonts/remixicon.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="container max-w-full md:max-w-3xl lg:max-w-5xl 2xl:max-w-7xl mx-auto px-4">
+    <div className="container max-w-full md:max-w-3xl lg:max-w-6xl mx-auto px-4">
       <Navbar></Navbar>
       <App />
     </div>
