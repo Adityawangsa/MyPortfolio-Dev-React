8df58831dd32e11a641df1b544031090a11362ef
Adityawangsa <adityahawari63@gmail.com>
2026-05-16 14:30:03 +0700

    Menambahkan pre loader

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,20 @@ import "./style.css";
 
 // Memasukkan layout dan komponen
 import App from "./App.jsx";
+import PreLoader from "./components/PreLoader.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 
 // Penggunaan remixicon
 import "remixicon/fonts/remixicon.css";
 
+// Penggunaan animate css
+import 'animate.css';
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    {/* Animasi PreLoader */}
+    <PreLoader></PreLoader>
     <div className="container max-w-full md:max-w-3xl lg:max-w-6xl mx-auto px-4">
       <Navbar></Navbar>
       <App />
