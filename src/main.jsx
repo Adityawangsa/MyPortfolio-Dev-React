92ca896cc51af1442ff05ea9be758f604685789a
Adityawangsa <adityahawari63@gmail.com>
2026-05-16 10:57:28 +0700

    feat: menambahkan komponen footer

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./style.css";
 // Memasukkan layout dan komponen
 import App from "./App.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 // Penggunaan remixicon
 import "remixicon/fonts/remixicon.css";
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")).render(
     <div className="container max-w-full md:max-w-3xl lg:max-w-6xl mx-auto px-4">
       <Navbar></Navbar>
       <App />
+      <Footer></Footer>
     </div>
   </StrictMode>,
 );
